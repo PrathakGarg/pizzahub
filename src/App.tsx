b0cf5ab740322c6f72645cc11d7aaf7e7573b0ed
PrathakGarg <prathakgarg@gmail.com>
2023-04-13 19:36:36 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/header/header.component";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Pizza Hub</h1>
+    </div>
+  );
+};
+
+export default App;
